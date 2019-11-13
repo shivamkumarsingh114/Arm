057e88efb4fe3dcc9b71e74f1660c61f0ccdc129
Shivam Kumar Singh <shivamkumarsingh114@gmail.com>
2019-11-13 11:44:13 +0530

    Assignment 2

--- Assignment-1/Q5_IMT2016114.s
+++ Assignment-1/Q5_IMT2016114.s
@@ -1,5 +1,5 @@
 ;We can use a GPIO pin to take input from the board itself.
 ;However, we don't have access to the hardware so,
-;µVision simulates the logic behavior of on-chip digital I/O Ports trough
+;µVision simulates the logic behaviour of on-chip digital I/O Ports trough
 ;Dialogs from the Peripheral menu, which help to verify the peripheral settings.
 ;Input values can be entered directly into these dialogs.
